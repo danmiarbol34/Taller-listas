5049674dc96379528d4d1b65ffe4f909c562f4a1
Juan Mangones <juan_291999@outlook.com>
2026-09-09 20:02:26 -0500

    modelo de estudiante

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -9,5 +9,68 @@ package modelo;
  * @author omar5
  */
 public class Estudiante {
+
+    private long identificacion;
+    private String nombre, sexo;
+    private int edad, grado;
+    private Acudiente acudiente;
+
+    public Estudiante(long identificacion, String nombre, String sexo, int edad, int grado, Acudiente acudiente) {
+        this.identificacion = identificacion;
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.grado = grado;
+        this.acudiente = acudiente;
+    }
+
+    public long getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(long identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getGrado() {
+        return grado;
+    }
+
+    public void setGrado(int grado) {
+        this.grado = grado;
+    }
+
+    public Acudiente getAcudiente() {
+        return acudiente;
+    }
+
+    public void setAcudiente(Acudiente acudiente) {
+        this.acudiente = acudiente;
+    }
+    
     
 }
