6f0251273fbaf9662e77e561ac4f24eab839c23d
Omar Daniel <panoliteam45@gmail.com>
2026-09-09 19:53:07 -0500

    modelo de acudiente y clase estudiante vacia

---
+++ src/modelo/Acudiente.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author omar5
+ */
+public class Acudiente {
+
+    private String Nombre, telefono;
+
+    public Acudiente(String Nombre, String telefono) {
+        this.Nombre = Nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getInfo() {
+        String Info = "";
+        Info = "dato del acudiente:\n"
+                + "Nombre: " + getNombre()
+                + "\n Telefono: " + getTelefono();
+        return Info;
+
+    }
+}
